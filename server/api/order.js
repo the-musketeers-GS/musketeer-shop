97775c11ea4f12971d2dc5750513f694d8ab6c5b
Adam Sisk <adamsisk91@gmail.com>
2019-01-08 11:19:07 -0600

    define userId in order api query

--- server/api/order.js
+++ server/api/order.js
@@ -1,17 +1,19 @@
-const router = require('express').Router()
-const {Order} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const { Order } = require('../db/models');
+module.exports = router;
 
-// GET /api/order/  return all products in an order where completed = false
+// GET /api/order/  return all products in an order where completed = false (current cart items)
 router.get('/', async (req, res, next) => {
+  const { userId } = req.body;
   try {
     const products = await Order.findAll({
       where: {
-        completed: false
+        completed: false,
+        userId
       }
-    })
-    res.json(products)
+    });
+    res.json(products);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
