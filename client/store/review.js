0a60a40af0b55d44ed1ddb6f3f6787cc4d38dac6
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-08 16:53:44 -0600

    create review reducer and add to the combine reducers

---
+++ client/store/review.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+//action contstant
+const GOT_ONE_PROJECT_REVIEWS_FROM_SERVER =
+  'GOT_ONE_PROJECT_REVIEWS_FROM_SERVER';
+
+//action type
+export const gotOneProjectReviewsFromServer = projReviews => {
+  return {
+    type: GOT_ONE_PROJECT_REVIEWS_FROM_SERVER,
+    projReviews: projReviews
+  };
+};
+
+//thunk creator
+export const fetchReviewsForOneProject = projectId => {
+  return async dispatch => {
+    const response = await axios.get(`/api/reviews/project/:${projectId}`);
+    const projReviews = response.data;
+    const action = gotOneProjectReviewsFromServer(projReviews);
+    dispatch(action);
+  };
+};
+
+//reducer
+
+const initialState = {
+  allProjReviews: []
+};
+
+const reviewReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_ONE_PROJECT_REVIEWS_FROM_SERVER:
+      return {
+        ...state,
+        allProjReviews: action.projReviews
+      };
+    default:
+      return state;
+  }
+};
+
+export default reviewReducer;
