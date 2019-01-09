4bcdea6e47e9ff900684f667b6934613dc6e09e8
Adam Sisk <adamsisk91@gmail.com>
2019-01-09 13:37:09 -0600

    commiting for code review

--- server/api/cart.js
+++ server/api/cart.js
@@ -16,11 +16,34 @@ router.get('/:cartId/items', async (req, res, next) => {
   }
 });
 
-// POST /api/cart/  create cart item
-// router.post('/:id', async (req, res, next) => {
-//   const id = req.params.id;
-//   try {
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+// POST /api/cart/:userId/:productId  create cart item
+router.post('/:userId/:productId', async (req, res, next) => {
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  // 1. check if user is logged in (later)
+  // 2. if they are, check if they have an exisiting cart, findOne where customer id
+  try {
+    let cartId;
+    const cart = await Cart.findOne({
+      where: {
+        userId
+      }
+    });
+    if (cart) {
+      cartId = cart.id;
+    }
+    // 3. if they DO have cart, find or create the cartItem with params/productId and current cart id
+    // 3.1 if they DON'T have cart, build one and set userId and save
+    if (!cart) {
+      const newCart = Cart.build();
+      newCart.userId = userId;
+      await newCart.save();
+      cartId = newCart.id;
+      console.log('NO CART', cartId);
+    }
+    // 4. if that item already exists, update quantity
+    res.sendStatus(418);
+  } catch (err) {
+    next(err);
+  }
+});
