7620e196c437b84729f7fa753eec6719f1ded5d0
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-09 09:15:08 -0600

    part way through creating review and review list

--- client/components/ReviewList.js
+++ client/components/ReviewList.js
@@ -1,5 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Review from './Review';
 
-class ReviewList extends React.Component {}
+class ReviewList extends React.Component {
+  componentDidMount() {
+    const projId = this.props.match.params.id;
+  }
 
-export default ReviewList
+  render() {
+    return <div>Reviews</div>;
+  }
+}
+const mapStateToProps = dispatch => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {};
+
+const ConnectedReviewList = connect(mapStateToProps, mapDispatchToProps)(
+  ReviewList
+);
+
+export default ConnectedReviewList;
