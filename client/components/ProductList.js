7e89cdf089d8bb9125ee14153dbc3eebdc83d9f8
christina0jin <chrisitna0jin@gmail.com>
2019-01-09 18:08:47 -0600

    created FilterByCategoryButton

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -7,6 +7,19 @@ const ProductList = props => {
   const { products } = props;
   return (
     <div>
+      <div className="dropdown">
+        <button type="button" className="dropbtn">
+          Filter By Category >>{' '}
+        </button>
+        <div className="dropdown-content">
+          <span>Accessories</span>
+          <span>Boots</span>
+          <span>Clothes</span>
+          <span>Hats</span>
+          <span>Weapons</span>
+        </div>
+      </div>
+
       {products.map(product => (
         <ul key={product.id}>
           <Link to={`/products/${product.id}`}>
