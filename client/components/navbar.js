b7856e5348039ebb7f82dac6c072c21448d6e035
Adam Sisk <adamsisk91@gmail.com>
2019-01-11 14:02:27 -0600

    navbar styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,42 +7,35 @@ import { logout } from '../store';
 import { toggleCart } from '../store/cart';
 import Cart from './Cart';
 import Title from './styles/Title';
-
-const StyledNav = styled.div`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-self: end;
-  font-size: 2rem;
-`;
+import NavStyles from './styles/NavStyles';
 
 const Navbar = ({ handleClick, isLoggedIn, openCart }) => (
-  <div>
+  <div style={{ justifyContent: 'center' }}>
     <Link to="/">
       <Title>MUSKETEER SHOP</Title>
     </Link>
-    <StyledNav>
+    <NavStyles>
       {isLoggedIn ? (
-        <div>
+        <NavStyles>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-        </div>
+        </NavStyles>
       ) : (
-        <div>
+        <NavStyles>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-        </div>
+        </NavStyles>
       )}
       {/* <Link to="/cart">Cart 🛒</Link> */}
       <button type="button" onClick={openCart}>
         Cart 🛒
       </button>
       <Cart />
-    </StyledNav>
+    </NavStyles>
     <hr />
   </div>
 );
