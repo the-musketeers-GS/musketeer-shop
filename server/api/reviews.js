f1318c63384548bfe7a392c7738d1bbef5acde93
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-11 14:48:46 -0600

    first post for review of product

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Review } = require('../db/models');
+const { Review, Product, User } = require('../db/models');
 module.exports = router;
 
 router.get('/product/:id', async (req, res, next) => {
@@ -8,6 +8,19 @@ router.get('/product/:id', async (req, res, next) => {
       where: { productId: req.params.id }
     });
     res.json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/product/:id', async (req, res, next) => {
+  try {
+    let product = await Product.findOne({
+      where: { id: req.params.id }
+    });
+    let review = await Review.create(req.body);
+    let productReview = await review.setProduct(product);
+    res.status(201).send(productReview);
   } catch (error) {
     next(error);
   }
