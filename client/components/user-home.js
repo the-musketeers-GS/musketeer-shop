8f9856ef2556a414f996e539b1fe099b52d214b2
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-13 20:11:43 -0600

    added user infor to the logged in bome page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,34 +1,65 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  console.log('props---------', props);
+  const {
+    email,
+    firstName,
+    lastName,
+    addr1,
+    addr2,
+    city,
+    state,
+    zipCode,
+    phone
+  } = props;
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <h3>
+        Welcome, {firstName} {lastName}
+      </h3>
+      <div>
+        <div>Address: </div>
+        <div>{addr1}</div>
+        <div>{addr2}</div>
+        <div>
+          {city}, {state} {zipCode}
+        </div>
+        <div>Email: {email}</div>
+        <div>Phone: {phone}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    email: state.user.email
-  }
-}
+    email: state.user.email,
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+    addr1: state.user.addr1,
+    addr2: state.user.addr2,
+    city: state.user.city,
+    state: state.user.state,
+    zipCode: state.user.zipCode,
+    phone: state.user.phone
+  };
+};
 
-export default connect(mapState)(UserHome)
+export default connect(mapState)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
   email: PropTypes.string
-}
+};
