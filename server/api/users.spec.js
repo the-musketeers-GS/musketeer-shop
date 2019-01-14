95fa6c93b4c0a533b4b6bc0af8b39e820999bca1
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-13 19:37:48 -0600

    fixed 2 tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,32 +1,40 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const User = db.model('user')
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
+const User = db.model('user');
 
 describe('User routes', () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    return db.sync({ force: true });
+  });
 
   describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
+    const codysEmail = 'cody@puppybook.com';
 
     beforeEach(() => {
       return User.create({
-        email: codysEmail
-      })
-    })
+        email: codysEmail,
+        firstName: 'Cody',
+        lastName: 'Puggy',
+        addr1: '555 Puppy Dog Lane',
+        city: 'Moline',
+        state: 'IL',
+        zipCode: '61240',
+        phone: '123456789',
+        isAdmin: false
+      });
+    });
 
     it('GET /api/users', async () => {
       const res = await request(app)
         .get('/api/users')
-        .expect(200)
+        .expect(200);
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
-    })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+      expect(res.body).to.be.an('array');
+      expect(res.body[0].email).to.be.equal(codysEmail);
+    });
+  }); // end describe('/api/users')
+}); // end describe('User routes')
