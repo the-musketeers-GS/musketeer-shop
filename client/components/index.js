52d1a540084c83cc7b9482b6e8f00c8f0cdced6f
christina0jin <chrisitna0jin@gmail.com>
2019-01-14 09:18:01 -0600

    guest cart component

--- client/components/index.js
+++ client/components/index.js
@@ -11,6 +11,7 @@ export { default as UpdateProduct } from './UpdateProduct';
 export { default as ManageProductList } from './ManageProductList';
 export { default as UserHome } from './user-home';
 export { default as Cart } from './Cart';
+export { default as GuestCart } from './GuestCart';
 export { default as ConnectedReviewList } from './ReviewList';
 export { default as ReviewList } from './ReviewList';
 export { default as SingleOrder } from './SingleOrder';
